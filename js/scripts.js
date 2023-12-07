f7886826e20d04128085f96d442308cd51316b0c
paulasiltor <paula23040@gmail.com>
2023-12-07 16:46:14 -0300

    subida parcial - css e js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
- 
+ //Selecao de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("edit-input");
+const cancelEditBtn = document.querySelector("cancel-edit-btn");
+
+
+ //Funcoes
+
+ //Eventos
